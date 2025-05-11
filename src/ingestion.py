fe76aefe9534fb40219a0a2b1b0dadbbe9a53b00
Hannah Courtney <hannahcourtney@hannahs-air.home>
2025-05-11 17:16:52 +0100

    Feature to ingest data into pipeline

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,3 +1,27 @@
+""" Module to ingest data into the pipeline from the source."""
+import os
+import json
+
+
+def load_fhir_data(data_dir: str):
+   """
+   Loads FHIR patient data from JSON files in a specified directory.
+
+   Args:
+       data_dir (str): The path to the directory containing the JSON files.
+
+   Returns:
+       list: A list of dictionaries representing the FHIR patient data.
+   """
+   data = []
+   for filename in os.listdir(data_dir):
+       if filename.endswith(".json"):
+           filepath = os.path.join(data_dir, filename)
+           with open(filepath) as f:
+               file_data = json.load(f)
+               data.append(file_data)
+  
+   return data
 
 
 
---
+++ tests/test_ingestion.py
@@ -0,0 +1,45 @@
+""" Module to test data ingestion. """
+from ingestion import load_fhir_data
+
+
+def test_load_fhir_data_full():
+   """
+   Test for loading FHIR data from a directory containing valid JSON files.
+
+   This test ensures that the `load_fhir_data()` function correctly loads
+   patient data from a specified directory containing valid JSON files.
+
+   It asserts that the returned list contains the correct data structure
+   based on a sample valid JSON file.
+   """
+   test_dir_full = "tests/data/test_raw"
+  
+   patient_data = load_fhir_data(test_dir_full)
+
+   expected_data = {
+       "patient_id": "1",
+       "name": [{"family": "Doe", "given": ["John"]}],
+       "gender": "male",
+       "birthDate": "1999-10-01"
+   }
+   assert patient_data[0] == expected_data
+
+
+def test_load_fhir_data_empty():
+   """
+   Test case for loading FHIR data from an empty directory.
+
+
+   This test ensures that the `load_fhir_data()` function returns an empty
+   list when given a directory that does not contain any JSON files or is empty.
+
+   It asserts that if the directory is empty or contains no valid JSON files,
+   the returned list will be empty.
+
+   This test is useful to check the behaviour of the function when there is no data to process.
+   """
+   test_dir_empty = "tests/data/test_raw_empty"
+  
+   patient_data = load_fhir_data(test_dir_empty)
+
+   assert patient_data == []
